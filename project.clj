56332e679cf2475c851ee1a05b8153fe8eaddd6f
Ilja Sidoroff <ilja.sidoroff@iki.fi>
2014-04-16 21:47:08 +0300

    Bumped up clojure version, added legal player to project

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject ggp-clj-players "0.1.1"
-  :description "CLojure library to develop GGP players"
+  :description "Clojure library to develop GGP players"
   :url "http://E"
   :license {:name "MIT license"
             :url "http://opensource.org/licenses/MIT"}
@@ -7,9 +7,9 @@
   :plugins [[lein-sub "0.3.0"]]
   :source-paths ["src"]
   :sub ["vendor/ggp-base"]
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [ggp-base "0.1.1"]]
-  :profiles {:uberjar {:aot [ggp-clj-players.random]}}
+  :profiles {:uberjar {:aot [ggp-clj-players.random ggp-clj-players.legal]}}
   :aliases {"kiosk" ["do" "clean" ["uberjar"] ["run" "-m" "ggp-clj-players.core/kiosk"]]
-            "launch" ["do" "clean" ["uberjar"] ["run" "-m" "ggp-clj-players.core/play"]]}
+            "launch" ["do" "clean" ["uberjar"] ["run" "-m" "ggp-clj-players.core/play2"]]}
   )
